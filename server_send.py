2381dab6a32dbbabfdf28871974389c1c01939ba
Körmöczi Gergő Gábor <h590114@stud.u-szeged.hu>
2026-05-13 13:16:49 +0200

    sender

--- server_send.py
+++ server_send.py
@@ -1,8 +1,27 @@
+import socket
+
+
+
 def main():
-    pass
+    # 1. Create the socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 2. Connect to the Server's IP
+    # Replace '192.168.1.XX' with the actual IP of the Host laptop
+    client_socket.connect(('192.168.1.XX', 5005))
+
+    # 3. Send data (Must be bytes!)
+    message = "Motion Detected!"
+    client_socket.send(message.encode('utf-8'))
+
+    # 4. Close
+    client_socket.close()
 
 
 
+
+
+    
 
 if __name__ == "__main__":
     main()
