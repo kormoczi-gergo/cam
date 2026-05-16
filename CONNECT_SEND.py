d79cb86b1e5078a1600d7ea2e9c783becf3b177c
Körmöczi Gergő Gábor <h590114@stud.u-szeged.hu>
2026-05-16 10:56:54 +0200

    refactorv0

--- CONNECT_SEND.py
+++ CONNECT_SEND.py
@@ -57,7 +57,7 @@ def main():
 
     ### STRONING IMAGE/S IN ARRAY ###
     #                          image(np arr)   |frame counter
-    recieved_image_pointer = [None,             0]
+    recorded_image_pointer = [None,             0]
     #array so it is a pointer, and dinamically changes no matter the scope, [0]: main image
 
 
