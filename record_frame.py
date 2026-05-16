8c90a4fd3b12c331b23866bf24d46081860e752e
Körmöczi Gergő Gábor <h590114@stud.u-szeged.hu>
2026-05-16 11:12:59 +0200

    refactorv0

--- record_frame.py
+++ record_frame.py
@@ -1,8 +1,8 @@
+import time
 
 
 
-
-
+#REVER IS BOTTLENECK: by the time 1000 images was taken the screen could only display like 600
 
 
 
@@ -25,10 +25,13 @@ def record_flow(cam, recorded_image_pointer):
         # cam is the object that takes photos
         # image1 is inputted so it can be changed
         while True:
-            
+
             if recorded_image_pointer[1]: # if connection established
 
                 recorded_image_pointer[0] = take_image(cam)
                 recorded_image_pointer[2] += 1 #inc frame counter with every shot
 
                 print(recorded_image_pointer[2])
+
+
+            time.sleep(0.015) #60fps
